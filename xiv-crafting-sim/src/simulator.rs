5fb946e88603cdd5f3f6c9514538ca92bbd9f65b
Chew <chw111111@hotmail.com>
2021-12-18 17:47:31 -0700

    added actual things

---
+++ xiv-crafting-sim/src/simulator.rs
@@ -0,0 +1,19 @@
+use crate::actions::Action;
+
+struct CrafterResult {
+    ability_sequence: Vec<Action>,
+
+}
+
+
+#[wasm_bindgen]
+struct CraftSimulator {
+
+}
+
+impl CraftSimulator {
+    #[wasm_bindgen]
+    fn step() {
+
+    }
+}
